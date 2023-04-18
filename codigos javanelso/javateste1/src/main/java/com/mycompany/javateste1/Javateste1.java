b026f3cf71d362bbd0ddc6d0cb3aab517b1cc81e
AlvaroDias1 <130410571+AlvaroDias1@users.noreply.github.com>
2023-04-17 21:52:51 -0300

    Add files via upload

---
+++ codigos javanelso/javateste1/src/main/java/com/mycompany/javateste1/Javateste1.java
@@ -0,0 +1,19 @@
+package com.mycompany.javateste1;
+
+import java.util.Scanner;
+
+public class Javateste1 {
+
+    public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        Scanner t2 = new Scanner(System.in);
+       
+        String nome;
+        String cpf;
+        
+    System.out.println("Informe o seu nome:");
+        nome = t.nextLine();
+    System.out.println("Informe o cpf:");
+        cpf = t2.nextLine();
+    }
+}
